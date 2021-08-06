6b490ef1e3133066b65d73e4c9753ed19a9aee4b
Stephen Chang <stchang216@gmail.com>
2021-08-06 19:01:37 +0000

    dont duplicate src code; read directly from files

--- src/app.rkt
+++ src/app.rkt
@@ -1,6 +1,10 @@
 #lang racketscript/base
 
-(require "./counter.rkt"
+(require (for-syntax racket/base
+                     racket/file
+                     racket/syntax
+                     syntax/parse)
+         "./counter.rkt"
          "./todo.rkt"
          rackt)
 
@@ -59,109 +63,27 @@
             (<> counter-example)
             (<> todo-example)))
 
-(define todo-source-code "#lang racketscript/base
+(require (for-syntax mzlib/etc))
+(define-for-syntax this-dir (this-expression-source-directory))
 
-(require racketscript/interop
-         \"./rackt.rkt\")
+;; get src code strs, at compile time, from actual file
+(define-syntax define-src-code-str-def
+  (syntax-parser
+    [(_ name:id)
+     #:with def-name (format-id #'name "~a-source-code" #'name)
+     #:with str-name (format-id #'name "~a-src-code-str" #'name)
+     #`(define def-name #,(syntax-local-value #'str-name))]))
 
-(define StateContext (create-context))
+;; TODO:
+;; need get these first, rather than in the macro,
+;; bc racketscript seems to expand twice?
+(define-syntax counter-src-code-str
+  (file->string (build-path this-dir "counter.rkt")))
+(define-syntax todo-src-code-str
+  (file->string (build-path this-dir "todo.rkt")))
 
-(define (add-todo state action)
-    (append ($ state 'todos) (list ($ action 'todo))))
-
-(define (remove-todo state action)
-    (filter (lambda (el) (not (eq? ($ el 'id) ($ action 'id)))) ($ state 'todos)))
-
-(define (reducer state action)
-    (cond
-        [(eq? ($ action 'type) \"add\")
-            ([ todos (add-todo state action)])]
-        [(eq? ($ action 'type) \"remove\")
-            ([ todos (remove-todo state action)])]
-        [else state]))
-
-(define (todo-input props . ..)
-    (define ctx (use-context StateContext))
-    (define dispatch ($ ctx 'dispatch))
-    (define store ($ ctx 'store))
-    (define-values (text set-text) (use-state \"\"))
-
-    (define (update-text e)
-        (set-text (js-string->string ($ e 'target 'value)) text))
-
-    (define (submit-todo e)
-        (($ e 'preventDefault))
-        (dispatch ([ type \"add\" ]
-                       [ todo ([ id (#js*.Date.now) ]
-                                     [ text text ])]))
-
-    (set-text \"\"))
-
-    (<> \"form\"
-         #:props ([ onSubmit submit-todo ])
-            (<> \"input\"
-                #:props ([ className \"todo-input\" ]
-                               [ placeholder \"What needs to be done?\" ]
-                               [ value text ]
-                               [ onChange update-text]))))
-
-(define (todo-item props . ..)
-    (define ctx (use-context StateContext))
-    (define dispatch ($ ctx 'dispatch))
-    (define (remove-todo id)
-        (dispatch ([ type \"remove\" ]
-                         [ id ($ props 'todo 'id) ])))
-
-    (<> \"li\"
-        #:props ([ className \"todo-item\"])
-            ($ props 'todo 'text)
-            (<> \"button\"
-                #:props ([ type \"button\" ]
-                         [ className \"button button-clear todo-remove-button\"]
-                         [ onClick remove-todo ])
-                \"x\")))
-
-(define (todo-list props . ..)
-    (define ctx (use-context StateContext))
-    (define dispatch ($ ctx 'dispatch))
-    (define store ($ ctx 'store))
-
-    (<> \"ul\"
-         (map (lambda (todo) (<> todo-item #:props ([todo todo]))) ($ store 'todos))))
-
-(define (todo-app props . ..)
-    (define provider ($ StateContext 'Provider))
-    (define default-state ([todos (list)]))
-    (define-values (store dispatch) (use-reducer reducer default-state))
-
-    (<> provider
-        #:props ([ value
-            ([ store store ]
-                   [ dispatch dispatch ])])
-        (<> \"div\"
-            (<> todo-input))
-            (<> todo-list)))
-
-(provide todo-app)
-")
-
-(define counter-source-code "(define (counter props ..)
-    (define-values (counter set-counter) (use-state 0))
-
-    (<> \"div\"
-        (<> \"button\"
-            #:props ([ className \"button\" ]
-                     [ type \"button\" ]
-                     [onClick (lambda (_) (set-counter (- counter 1)))])
-            \"- 1\")
-
-        (<> \"span\" #:props ([ className \"counter\" ]) counter)
-
-        (<> \"button\"
-            #:props ([ className \"button\" ]
-                     [ type \"button\" ]
-                     [onClick (lambda (_) (set-counter (+ counter 1)))])
-            \"+ 1\")))")
+(define-src-code-str-def todo)
+(define-src-code-str-def counter)
 
 (render (<> app) "root")
 
